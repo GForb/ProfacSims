35b3ae5383f5d55ffa41e4056c3ae4de281739d1
GForb <48352959+GForb@users.noreply.github.com>
2023-12-14 16:47:15 +0000

    editing test to use correct data

--- tests/testthat/test-methods_sim_reulsts.R
+++ tests/testthat/test-methods_sim_reulsts.R
@@ -4,7 +4,7 @@ test_that("prep_data_for_plot", {
   db <- DBI::dbConnect(RSQLite::SQLite(), here::here("Results/Database/sim_results.db"))
   data <- DBI::dbGetQuery(db,
                      "SELECT n_studies, ICC, R2, study_sample_size_train ,test_ss, int_pred_corr, model, est, tau2, sigma_e, sigma_u, intercept_est_sample_size, metric, n_studies_test, rep_number
-            FROM sim_results_v1
+            FROM sim_results_v2
             WHERE sim_name = 'BatchAug2023' AND metric = 'calib_itl' AND model = 'Fixed intercept' AND ICC = 0 AND R2 = 0.4 AND test_ss = 300 AND int_pred_corr = 0.0 AND n_studies = 4 AND study_sample_size_train = 50"
   )
 
@@ -21,7 +21,7 @@ test_that("sim_results_lazy_stack", {
   db <- DBI::dbConnect(RSQLite::SQLite(), here::here("Results/Database/sim_results.db"))
   data <- DBI::dbGetQuery(db,
                      "SELECT n_studies, ICC, R2, study_sample_size_train ,test_ss, int_pred_corr, model, est, tau2, sigma_e, sigma_u, intercept_est_sample_size, metric, n_studies_test, rep_number, error_var_u
-            FROM sim_results_v1
+            FROM sim_results_v2
             WHERE sim_name = 'BatchAug2023' AND model = 'Fixed intercept' AND ICC = 0 AND R2 = 0.4 AND test_ss = 300 AND int_pred_corr = 0.0 AND n_studies = 4 AND study_sample_size_train = 50"
   )
 
