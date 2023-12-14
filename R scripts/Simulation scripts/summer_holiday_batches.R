eb3c5cdccae778f29ae486b7cef6c650b36a4178
GForb <48352959+GForb@users.noreply.github.com>
2023-12-14 16:48:23 +0000

    moving summer holiday batch script, no edits

---
+++ R scripts/Simulation scripts/summer_holiday_batches.R
@@ -0,0 +1,29 @@
+install_github("GForb/ProfacSims", upgrade= "never")
+
+
+library(ProfacSims)
+library(furrr)
+library(tictoc)
+
+sim_params <- list(
+  nreps = 10,
+  sim_rep_fun = list(ProfacSims:::sim_rep_continuous_new_test_studies),
+  n_studies = c(4, 8, 16, 32, 64),
+  model_function_list = list(list(ProfacSims:::model_lm_fixed_int,
+                                  ProfacSims:::model_lm,
+                                  ProfacSims:::model_lmm_random_int_reml,
+                                  ProfacSims:::model_lmm_random_int_ml)),
+  study_sample_size_train = c(50, 200, 1000),
+  study_sample_size_test = 5000,
+  ICC = c(0,0.05, 0.3),
+  R2 = c(0.4, 0.7),
+  int_pred_corr = c(0, 0.5),
+  intercept_est_sample_size = list(c(10,100, 1000)),
+  n_studies_test = 30
+)
+
+
+
+tic()
+ProfacSims:::run_simualtions_in_batches(sim_params = sim_params, n_batches = 20)
+toc()
