f1e223fc9b8eea0dbcbe6c1b527eb5c8482ff04e
GForb <48352959+GForb@users.noreply.github.com>
2024-01-12 17:11:54 +0000

    deleing 'evaluate_performance_continuous_old' function

--- R/performance_metrics.R
+++ R/performance_metrics.R
@@ -97,18 +97,6 @@ metric_rsqared_old <- function(predicted_lp, observed_outcome) {
 }
 
 
-evaluate_performance_continuous_old <- function(test_data, model) {
-  outcome <- names(stats::model.frame(model))[1]
-  observed_outcome <- test_data[, outcome]
-  predicted_lp <- predict(model, newdata = test_data)
-  rbind(
-    metric_calib_slope_cont(predicted_lp, observed_outcome),
-    metric_calib_itl_cont(predicted_lp, observed_outcome),
-    metric_rsqared(predicted_lp, observed_outcome),
-    metric_rsqared_old(predicted_lp, observed_outcome),
-    make.row.names = FALSE
-   )
-}
 
 
 evaluate_performance_continuous <- function(test_data, model, new_studies = FALSE) {
@@ -166,6 +154,12 @@ evaluate_performance_continuous_new_studies <- function(test_data, model) {
 
 evaluate_performance_continuous_new_studies0 <- function(test_data, model) {
   outcome <- names(stats::model.frame(model))[1]
+  # for random intercept models prediction for unseen clusters defaults to using zero intercept for new clusters. For linear models a different approach is required.
+  if("lm" %in% class(model)) {
+    predicted_lp <- predict(model, newdata = test_data, type = "response")
+  } else {
+    predicted_lp <- predict(model, newdata = test_data)
+  }
 
 
 
