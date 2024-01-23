4715a7d7ed64303d18f6b46517c8198695af318b
GForb <48352959+GForb@users.noreply.github.com>
2024-01-23 11:14:57 +0000

    updating save scripts

--- R scripts/save_pred-new-data.R
+++ R scripts/save_pred-new-data.R
@@ -3,14 +3,16 @@ library(RSQLite)
 
 db <- dbConnect(RSQLite::SQLite(), "Results/Database/sim_results.db")
 
+# Drop table if required
+# dbExecute(db,"DROP TABLE sim_results_v4")
 # drop previously saved results
-#dbExecute(db, "DELETE FROM  sim_results_v4 WHERE sim_name = '160122-pred-new-data'")
+# dbExecute(db, "DELETE FROM  sim_results_v4 WHERE sim_name = '160122-pred-new-data'")
 
 
 
 save_cluster_results_db(db = db,
                       table = "sim_results_v4",
-                      sim_name = "160122-pred-new-data")
+                      sim_name = "240119-investigations")
 
 # Count rows in table
 dbGetQuery(db,
