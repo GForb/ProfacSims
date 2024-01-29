347ed0b3dff7bed5481fffa6e79c6318c9eafbdb
GForb <48352959+GForb@users.noreply.github.com>
2024-01-29 20:49:40 +0000

    reporting

--- R/table_functions.R
+++ R/table_functions.R
@@ -21,10 +21,10 @@ quantiles_table_tau <- function(data) {
   data |>
     mutate(tau = sqrt(tau2))
     summarise(
-      p005 = stats::quantile(est, probs = 0.05) |> round(digits = 2),
-      p25 = stats::quantile(est, probs = 0.25)|> round(digits = 2),
-      p50 = stats::quantile(est, probs = 0.50)|> round(digits = 2),
-      p75 = stats::quantile(est, probs = 0.75)|> round(digits = 2),
-      p95 = stats::quantile(est, probs = 0.95)|> round(digits = 2)
+      p005 = stats::quantile(tau, probs = 0.05) |> round(digits = 2),
+      p25 = stats::quantile(tau, probs = 0.25)|> round(digits = 2),
+      p50 = stats::quantile(tau, probs = 0.50)|> round(digits = 2),
+      p75 = stats::quantile(tau, probs = 0.75)|> round(digits = 2),
+      p95 = stats::quantile(tau, probs = 0.95)|> round(digits = 2)
     )
 }
