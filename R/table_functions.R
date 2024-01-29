adb5f8b248d135047bc07cdfea67f156d08a1ce2
GForb <48352959+GForb@users.noreply.github.com>
2024-01-29 13:58:52 +0000

    reporting updates

--- R/table_functions.R
+++ R/table_functions.R
@@ -8,15 +8,11 @@ quantile_df <- function(x, probs = c(0.25, 0.5, 0.75)) {
 quantiles_table_est <- function(data) {
   data |>
     summarise(
-      min = min(est) |> round(digits = 2),
-      p001 = stats::quantile(est, probs = 0.01) |> round(digits = 2),
       p005 = stats::quantile(est, probs = 0.05) |> round(digits = 2),
       p25 = stats::quantile(est, probs = 0.25)|> round(digits = 2),
       p50 = stats::quantile(est, probs = 0.50)|> round(digits = 2),
       p75 = stats::quantile(est, probs = 0.75)|> round(digits = 2),
-      p95 = stats::quantile(est, probs = 0.95)|> round(digits = 2),
-      p99 = stats::quantile(est, probs = 0.99) |> round(digits = 2),
-      max = max(est) |> round(digits = 2),
+      p95 = stats::quantile(est, probs = 0.95)|> round(digits = 2)
   )
 }
 
@@ -25,14 +21,10 @@ quantiles_table_tau <- function(data) {
   data |>
     mutate(tau = sqrt(tau2))
     summarise(
-      min = min(est) |> round(digits = 2),
-      p001 = stats::quantile(est, probs = 0.01) |> round(digits = 2),
       p005 = stats::quantile(est, probs = 0.05) |> round(digits = 2),
       p25 = stats::quantile(est, probs = 0.25)|> round(digits = 2),
       p50 = stats::quantile(est, probs = 0.50)|> round(digits = 2),
       p75 = stats::quantile(est, probs = 0.75)|> round(digits = 2),
-      p95 = stats::quantile(est, probs = 0.95)|> round(digits = 2),
-      p99 = stats::quantile(est, probs = 0.99) |> round(digits = 2),
-      max = max(est) |> round(digits = 2),
+      p95 = stats::quantile(est, probs = 0.95)|> round(digits = 2)
     )
 }
