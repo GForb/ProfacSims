f3dc46b24c8c70583c7351a199f55047f0674252
GForb <48352959+GForb@users.noreply.github.com>
2024-02-11 00:31:16 +0000

    Update box_plot-cluster-x.R

--- R/box_plot-cluster-x.R
+++ R/box_plot-cluster-x.R
@@ -12,6 +12,6 @@ box_plot_cluster_x <- function(data) {
     scale_x_continuous(breaks = c("0" = 1, "0.05" = 2, "0.2" = 3, "0.5" = 4, "0.9" = 5)) +
     xlab("Predictor ICC") +
     facet_grid(rows = vars(n_studies), cols = vars(b_w_ratio)) +
-    guides(color = guide_legend(nrow = 2))
+    guides(color = guide_legend(nrow = 2)) +
     theme(legend.position = "top")
 }
