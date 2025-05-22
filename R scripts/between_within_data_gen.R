1842bf8f8b030ea629eee0384f75b9ab975b2a64
GForb <48352959+GForb@users.noreply.github.com>
2025-05-22 15:26:20 +0100

    Reoriganising files

--- R scripts/between_within_data_gen.R
+++
@@ -1,7 +0,0 @@
-set.seed(1234)
-sigmas <- get_sigmas(n_predictors = 1, ICC = 0.5, R2 = 0.7, int_pred_corr = 0.5)
-data <- generate_continuous(n_studies = 100, study_sample_size = 50, n_predictors =1, sigmas = sigmas)
-
-  haven::write_dta(data, "Results/int_pred_corr.dta")
-
-  sigmas
